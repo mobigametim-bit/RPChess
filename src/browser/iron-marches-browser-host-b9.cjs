56e2648d15e492d6e91d8e619e40ba8c9a620f9d
mobigametim-bit <mobigametim@gmail.com>
2026-08-07 13:33:08 +0300

    Activate production events before B9 browser host loads

--- src/browser/iron-marches-browser-host-b9.cjs
+++ src/browser/iron-marches-browser-host-b9.cjs
@@ -1,6 +1,7 @@
 'use strict';
 
 require('./install-b9-map-runtime.cjs');
+require('./install-b9-production-events.cjs');
 require('./install-b9-profile-load.cjs');
 require('./install-b9-map-polish.cjs');
 module.exports = require('./iron-marches-browser-host.cjs');
