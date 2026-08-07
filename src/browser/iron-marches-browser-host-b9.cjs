9122d49e9e514108fce31e74c97e9420c00072e4
mobigametim-bit <mobigametim@gmail.com>
2026-08-07 15:11:19 +0300

    Install B10-B13 production runtime on browser host

--- src/browser/iron-marches-browser-host-b9.cjs
+++ src/browser/iron-marches-browser-host-b9.cjs
@@ -1,7 +1,9 @@
 'use strict';
 
+require('../runtime/install-production-economy-stageb.cjs');
 require('./install-b9-map-runtime.cjs');
 require('./install-b9-production-events.cjs');
 require('./install-b9-profile-load.cjs');
 require('./install-b9-map-polish.cjs');
+require('./install-b10-b13-production-runtime.cjs');
 module.exports = require('./iron-marches-browser-host.cjs');
