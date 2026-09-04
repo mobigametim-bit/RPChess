634a4449cbbc5cff25f220f5959620a9d3234ae5
mobigametim-bit <mobigametim@gmail.com>
2026-09-04 14:01:38 +0300

    i18n: aggregate event English dictionaries

---
+++ game/localization/events/en.mjs
@@ -0,0 +1,62 @@
+import { EVENT_EN_V3_01 } from './en-v3-01.mjs';
+import { EVENT_EN_V3_02 } from './en-v3-02.mjs';
+import { EVENT_EN_V3_03 } from './en-v3-03.mjs';
+import { EVENT_EN_V3_04 } from './en-v3-04.mjs';
+import { EVENT_EN_V3_05 } from './en-v3-05.mjs';
+import { EVENT_EN_V3_06 } from './en-v3-06.mjs';
+import { EVENT_EN_V3_07 } from './en-v3-07.mjs';
+import { EVENT_EN_V3_08 } from './en-v3-08.mjs';
+import { EVENT_EN_V3_09 } from './en-v3-09.mjs';
+import { EVENT_EN_V3_10 } from './en-v3-10.mjs';
+
+import { EVENT_EN_V4C_01 } from './en-v4c-01.mjs';
+import { EVENT_EN_V4C_02 } from './en-v4c-02.mjs';
+import { EVENT_EN_V4C_03 } from './en-v4c-03.mjs';
+import { EVENT_EN_V4C_04 } from './en-v4c-04.mjs';
+import { EVENT_EN_V4C_05 } from './en-v4c-05.mjs';
+import { EVENT_EN_V4C_06 } from './en-v4c-06.mjs';
+import { EVENT_EN_V4C_07 } from './en-v4c-07.mjs';
+import { EVENT_EN_V4C_08 } from './en-v4c-08.mjs';
+import { EVENT_EN_V4C_09 } from './en-v4c-09.mjs';
+import { EVENT_EN_V4C_10 } from './en-v4c-10.mjs';
+import { EVENT_EN_V4C_11 } from './en-v4c-11.mjs';
+import { EVENT_EN_V4C_12 } from './en-v4c-12.mjs';
+import { EVENT_EN_V4C_13 } from './en-v4c-13.mjs';
+import { EVENT_EN_V4C_14 } from './en-v4c-14.mjs';
+import { EVENT_EN_V4C_15 } from './en-v4c-15.mjs';
+import { EVENT_EN_V4C_16 } from './en-v4c-16.mjs';
+import { EVENT_EN_V4C_17 } from './en-v4c-17.mjs';
+import { EVENT_EN_V4C_18 } from './en-v4c-18.mjs';
+import { EVENT_EN_V4C_19 } from './en-v4c-19.mjs';
+import { EVENT_EN_V4C_20 } from './en-v4c-20.mjs';
+import { EVENT_EN_V4C_21 } from './en-v4c-21.mjs';
+import { EVENT_EN_V4C_22 } from './en-v4c-22.mjs';
+import { EVENT_EN_V4C_23 } from './en-v4c-23.mjs';
+import { EVENT_EN_V4C_24 } from './en-v4c-24.mjs';
+import { EVENT_EN_V4C_25 } from './en-v4c-25.mjs';
+
+import { EVENT_EN_V5_01 } from './en-v5-01.mjs';
+import { EVENT_EN_V5_02 } from './en-v5-02.mjs';
+import { EVENT_EN_V5_03 } from './en-v5-03.mjs';
+import { EVENT_EN_V5_04 } from './en-v5-04.mjs';
+import { EVENT_EN_V5_05 } from './en-v5-05.mjs';
+import { EVENT_EN_V5_06 } from './en-v5-06.mjs';
+import { EVENT_EN_V5_07 } from './en-v5-07.mjs';
+import { EVENT_EN_V5_08 } from './en-v5-08.mjs';
+import { EVENT_EN_V5_09 } from './en-v5-09.mjs';
+import { EVENT_EN_V5_10 } from './en-v5-10.mjs';
+import { EVENT_EN_V5_11 } from './en-v5-11.mjs';
+
+const EVENT_EN_EXACT = Object.freeze(Object.assign({},
+  EVENT_EN_V4C_01, EVENT_EN_V4C_02, EVENT_EN_V4C_03, EVENT_EN_V4C_04, EVENT_EN_V4C_05,
+  EVENT_EN_V4C_06, EVENT_EN_V4C_07, EVENT_EN_V4C_08, EVENT_EN_V4C_09, EVENT_EN_V4C_10,
+  EVENT_EN_V4C_11, EVENT_EN_V4C_12, EVENT_EN_V4C_13, EVENT_EN_V4C_14, EVENT_EN_V4C_15,
+  EVENT_EN_V4C_16, EVENT_EN_V4C_17, EVENT_EN_V4C_18, EVENT_EN_V4C_19, EVENT_EN_V4C_20,
+  EVENT_EN_V4C_21, EVENT_EN_V4C_22, EVENT_EN_V4C_23, EVENT_EN_V4C_24, EVENT_EN_V4C_25,
+  EVENT_EN_V3_01, EVENT_EN_V3_02, EVENT_EN_V3_03, EVENT_EN_V3_04, EVENT_EN_V3_05,
+  EVENT_EN_V3_06, EVENT_EN_V3_07, EVENT_EN_V3_08, EVENT_EN_V3_09, EVENT_EN_V3_10,
+  EVENT_EN_V5_01, EVENT_EN_V5_02, EVENT_EN_V5_03, EVENT_EN_V5_04, EVENT_EN_V5_05,
+  EVENT_EN_V5_06, EVENT_EN_V5_07, EVENT_EN_V5_08, EVENT_EN_V5_09, EVENT_EN_V5_10, EVENT_EN_V5_11
+));
+
+export { EVENT_EN_EXACT };
